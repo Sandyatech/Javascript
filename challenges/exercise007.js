70b4e6887571865eb18c6a4e43d84c55e69da8c6
sandyatech <sandhya.mskgmail.com>
2023-01-21 18:39:55 +0000

    replaced var and removed comments

--- challenges/exercise007.js
+++ challenges/exercise007.js
@@ -1,10 +1,6 @@
-/**
- * This function takes a number, e.g. 123 and returns the sum of all its digits, e.g 6 in this example.
- * @param {Number} n
- */
 export const sumDigits = (n) => {
     if (n === undefined) throw new Error("n is required");
-    var num = 0;
+    let num = 0;
     while (n > 0) {
         num += n % 10;
         n = Math.floor(n / 10);
@@ -12,117 +8,58 @@ export const sumDigits = (n) => {
     return num;
 };
 
-/**
- * This function creates a range of numbers as an array. It received a start, an end and a step. Step is the gap between numbers in the range. For example, if start = 3, end = 11 and step = 2 the resulting range would be: [3, 5, 7, 9, 11]
- * Both the start and the end numbers are inclusive.
- * Step is an optional parameter. If it is not provided, assume the step is 1.
- * @param {Number} start
- * @param {Number} end
- * @param {Number} step
- */
 export const createRange = (start, end, step) => {
     if (start === undefined) throw new Error("start is required");
     if (end === undefined) throw new Error("end is required");
     if (step === undefined)
         step = 1;
-    var range = [];
-    for (var i = start; i <= end; i = i + step)
+    const range = [];
+    for (let i = start; i <= end; i = i + step)
         range.push(i);
     return range;
 };
 
-/**
- * This function takes an array of user objects and their usage in minutes of various applications. The format of the data should be as follows:
- * [
- *  {
- *    username: "beth_1234",
- *    name: "Beth Smith",
- *    screenTime: [
- *                 { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
- *                 { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31} },
- *                 { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },
- *                 { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61} },
- *                ]
- *   },
- *   {
- *    username: "sam_j_1989",
- *    name: "Sam Jones",
- *    screenTime: [
- *                 { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
- *                 { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
- *                 { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
- *                ]
- *   },
- * ]
- *
- * The function should return an array of usernames of users who have used more than 100 minutes of screentime for a given date.
- * The date will be provided in the format "2019-05-04" (YYYY-MM-DD)
- * For example, if passed the above users and the date "2019-05-04" the function should return ["beth_1234"] as she used over 100 minutes of screentime on that date.
- * @param {Array} users
- */
 export const getScreentimeAlertList = (users, date) => {
     if (users === undefined) throw new Error("users is required");
     if (date === undefined) throw new Error("date is required");
     const sumValues = obj => Object.values(obj).reduce((a, b) => a + b, 0);
-    var user = [];
-    for (var i = 0; i < users.length; i++) {
-        for (var j = 0; j < users[i].screenTime.length; j++)
-            //console.log();
+    const user = [];
+    for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < users[i].screenTime.length; j++)
             if (users[i].screenTime[j].date === date)
                 if (sumValues(users[i].screenTime[j].usage) > 100)
                     user.push(users[i].username);
 
     }
-    //console.log(user)
     return user;
 };
 
-/**
- * This function will receive a hexadecimal color code in the format #FF1133. A hexadecimal code is a number written in hexadecimal notation, i.e. base 16. If you want to know more about hexadecimal notation:
- * https://www.youtube.com/watch?v=u_atXp-NF6w
- * For colour codes, the first 2 chars (FF in this case) represent the amount of red, the next 2 chars (11) represent the amound of green, and the last 2 chars (33) represent the amount of blue.
- * Colours can also be represented in RGB format, using decimal notation.
- * This function should transform the hex code into an RGB code in the format:
- * "rgb(255,17,51)"
- * Hint: You will need to convert each hexadecimal value for R, G and B into its decimal equivalent!
- * @param {String} str
- */
 export const hexToRGB = (hexStr) => {
     if (hexStr === undefined) throw new Error("hexStr is required");
-
     const hexToDecimal = hex => parseInt(hex, 16);
-    var colRed = hexStr.substr(1, 2);
-    var colGreen = hexStr.substr(3, 2);
-    var colBlue = hexStr.substr(5, 2);
+    const colRed = hexStr.substr(1, 2);
+    const colGreen = hexStr.substr(3, 2);
+    const colBlue = hexStr.substr(5, 2);
     return ("rgb(" + hexToDecimal(colRed) + "," + hexToDecimal(colGreen) + "," + hexToDecimal(colBlue) + ")");
 };
 
-/**
- * This function takes a noughts and crosses board represented as an array, where an empty space is represented with null.
- * [
- *  ["X", "0", null],
- *  ["X", null, "0"],
- *  ["X", null, "0"]
- * ]
- * The function should return "X" if player X has won, "0" if the player 0 has won, and null if there is currently no winner.
- * @param {Array} board
- */
 export const findWinner = (board) => {
     if (board === undefined) throw new Error("board is required");
-    var xCount = 0;
-    var oCount = 0;
-    var totalCount = 0;
-    for (var i = 0; i < board.length; i++)
-        for (var j = 0; j < board[i].length; j++) {
+    let xCount = 0;
+    let oCount = 0;
+    let winLimit = 0;
+    for (let i = 0; i < board.length; i++)
+        for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === "X")
                 xCount += 1;
             else if (board[i][j] === "0")
                 oCount += 1;
-            totalCount += 1;
+
         }
-    if (xCount > totalCount / 2)
+    winLimit = (xCount + oCount) / 2;
+    if (xCount > winLimit)
         return ("X");
-    else if (oCount > totalCount / 2)
+    else if (oCount > winLimit)
         return ("0");
     else
         return null;
