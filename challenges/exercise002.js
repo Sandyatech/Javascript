d3730c99d983f804edd3b7849fc25bdb3140b105
sandyatech <sandhya.mskgmail.com>
2023-01-21 18:33:40 +0000

    Optimized countsheep and replaced var

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,19 +1,16 @@
 export function getFillings(sandwich) {
     if (sandwich === undefined) throw new Error("ingredients is required");
-    // Your code here!
     return (sandwich.fillings);
 }
 
 export function isFromManchester(person) {
     if (person === undefined) throw new Error("person is required");
-    // Your code here! 
     return (person.city === "Manchester");
 }
 
 export function getBusNumbers(people) {
     if (people === undefined) throw new Error("people is required");
-    // Your code here!
-    var noOfBuses = 1;
+    let noOfBuses = 1;
     while (people > 40) {
         people = people - 40;
         noOfBuses++;
@@ -23,20 +20,11 @@ export function getBusNumbers(people) {
 
 export function countSheep(arr) {
     if (arr === undefined) throw new Error("arr is required");
-    // Your code here!
-    var noOfSheep = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === "sheep")
-            noOfSheep++;
-    }
-
-    return noOfSheep;
+    return (arr.filter(animalName => animalName === "sheep").length);
 
 }
 
 export function hasMPostCode(person) {
     if (person === undefined) throw new Error("person is required");
-    // Your code here!
-    //console.log(person["address"]["postCode"].charAt(0));
     return ((person.address.postCode.charAt(0) === "M") && (person.address.city === "Manchester"));
 }
