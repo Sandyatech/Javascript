82436d8d89d15ffb546ae9e491d9b0176cf3a301
sandyatech <sandhya.mskgmail.com>
2023-01-21 18:32:36 +0000

    Optimized reverseWord,reverseAllWords and replaced var

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,8 +1,5 @@
-// Note: Be sure to check out all the exercises corresponding .md files (in docs)! 📘 👍
-
 export function capitalize(word) {
     if (word === undefined) throw new Error("word is required");
-    // Add your code here! 
     return (word.charAt(0).toUpperCase() + word.slice(1));
 
 }
@@ -10,82 +7,50 @@ export function capitalize(word) {
 export function generateInitials(firstName, lastName) {
     if (firstName === undefined) throw new Error("firstName is required");
     if (lastName === undefined) throw new Error("lastName is required");
-    // Add your code here!
     return (firstName.charAt(0).toUpperCase() + "." + lastName.charAt(0).toUpperCase());
 }
 
 export function addVAT(originalPrice, vatRate) {
     if (originalPrice === undefined) throw new Error("originalPrice is requied");
     if (vatRate === undefined) throw new Error("vatRate is required");
-    // Add your code here!
     return (Math.round(originalPrice * (100 + vatRate)) / 100);
 }
 
 export function getSalePrice(originalPrice, reduction) {
     if (originalPrice === undefined) throw new Error("originalPrice is required");
     if (reduction === undefined) throw new Error("reduction is required");
-    // Add your code here!
     return (Math.round(originalPrice * (100 - reduction)) / 100);
 
 }
 
 export function getMiddleCharacter(str) {
     if (str === undefined) throw new Error("str is required");
-    // Add your code here!
-    if (str.length % 2 === 0) {
-        return (str.substr((str.length / 2) - 1, 2));
-    }
-    else {
-        return (str.charAt(Number(str.length / 2)));
-    }
-
+    return ((str.length % 2 === 0) ? str.substr((str.length / 2) - 1, 2) : str.charAt(Number(str.length / 2)));
 }
 
 export function reverseWord(word) {
     if (word === undefined) throw new Error("word is required");
-    // Add your code here!
     return (word.split("").reverse().join(""));
 }
 
 export function reverseAllWords(words) {
     if (words === undefined) throw new Error("words is required");
-    for (var i = 0; i < words.length; i++) {
-        words[i] = words[i].split("").reverse().join("");
-    }
-    return (words);
-    // Add your code here!
+    return (words.map(element => reverseWord(element)));
+
 }
 
 export function countLinuxUsers(users) {
-
     if (users === undefined) throw new Error("users is required");
-    // Add your code here!
-    /*for (var i = 0; i < users.length; i++) {
-        if (users[i]["type"] === "Linux")
-            count++;
-    }
-    return count;*/
-
-    var count = users.filter(user => { if (user.type === "Linux") return (user); });
-    return (count.length);
+    return (users.filter(user => user.type === "Linux").length);
 }
 
 export function getMeanScore(scores) {
     if (scores === undefined) throw new Error("scores is required");
-    // Add your code here!
     const add = (a, b) => a + b;
     return (Math.round((scores.reduce(add) / scores.length) * 100) / 100);
 }
 
 export function simpleFizzBuzz(n) {
     if (n === undefined) throw new Error("n is required");
-    // Add your code here!
-    if ((n % 3 === 0) && (n % 5 === 0))
-        return ("fizzbuzz");
-    else if ((n % 3 != 0) && (n % 5 != 0))
-        return (n);
-    else if ((n % 3 === 0))
-        return ("fizz");
-    else if ((n % 5 === 0))
-        return ("buzz");
+    return ((n % 3 === 0) ? ((n % 5 === 0) ? "fizzbuzz" : "fizz") : (n % 5 === 0) ? "buzz" : n);
 }
